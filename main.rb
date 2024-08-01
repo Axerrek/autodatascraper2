f3dd3fe0c6c500efc4739cc67641d6168111144a
Tomasz Wójcicki <62150847+Axerrek@users.noreply.github.com>
2024-08-01 08:07:03 +0200

    Add files via upload

--- main.rb
+++ main.rb
@@ -28,7 +28,7 @@ class Main
   def load_existing_links
     if File.exist?(@csv_filename)
       CSV.foreach(@csv_filename, headers: true, encoding: 'UTF-8') do |row|
-        @existing_links.add(row['Link'])  # Zakładam, że w pliku CSV jest kolumna o nazwie 'Link'
+        @existing_links.add(row['link'])  # Zakładam, że w pliku CSV jest kolumna o nazwie 'Link'
       end
     end
   end
@@ -38,32 +38,59 @@ class Main
   #
   # [parameters] Parametr do sprawdzenia.
   # [link] Link związany z parametrem.
-  def check_semicolons(parameters, link)
+  def check_semicolons(parameters, link, brand, model, fullname)
     semicolons = parameters.count(';')
     @mutex.synchronize do
       if semicolons > @max_semicolons
         @max_semicolons = semicolons
-        @max_semicolons_params = "link: #{link};#{parameters}"
+        @max_semicolons_params = "link: #{link};marka: #{brand};model: #{model};nazwa: #{fullname};#{parameters}"
       end
     end
   end
-
+  # Wyłuskuje markę, model oraz pełną nazwę z URL-a, zakładając, że URL zaczyna się od określonego base_url
+  #
+  # @param [String] link URL, z którego ma zostać wydobyta marka i model.
+  # @return [Array<String, String, String>] Tablica zawierająca:
+  #   - Markę wydobyta z URL-a (lub nil, jeśli URL nie pasuje do oczekiwanego wzorca).
+  #   - Model wydobyty z URL-a (lub nil, jeśli URL nie pasuje do oczekiwanego wzorca).
+  #   - Pozostałą część ścieżki URL-a po usunięciu base_url (lub nil, jeśli URL nie pasuje do oczekiwanego wzorca).
+  def extract_brand_and_model(link)
+    # Zakładamy, że link jest poprawny i ma odpowiednią strukturę
+    base_url = "https://www.autocentrum.pl/dane-techniczne/"
+    if link.start_with?(base_url)
+      # Usuń część base_url
+      path = link.sub(base_url, "")
+      # Rozdziel ścieżkę na części
+      parts = path.split("/")
+      # Zakładamy, że marka i model są na pierwszej i drugiej pozycji
+      brand = parts[0]
+      model = parts[1]
+      return [brand, model, path]
+    else
+      return [nil, nil, nil]
+    end
+  end
   ##
   # Pobiera parametry ze strony pod danym linkiem i przetwarza je.
   #
   # [link] Link do strony z parametrami.
   def process_link(link)
+    puts "link nieodrzucony?"
     return if @existing_links.include?(link)
+    puts "tak"
     @mutex.synchronize { @licznik += 1 }
+    brand, model, fullname = extract_brand_and_model(link)
     parameters = Fetcher.extract_parameters(link)
     if !parameters.nil? && !parameters.empty?
-      check_semicolons(parameters, link)
+      check_semicolons(parameters, link, brand, model, fullname)
       @mutex.synchronize do
-        @params += "link: #{link};#{parameters}\n"
+        @params += "link: #{link};marka: #{brand};model: #{model};nazwa: #{fullname};#{parameters}\n"
         puts @licznik
       end
     end
   end
+
+
 
   ##
   # Główna metoda uruchamiająca proces pobierania i przetwarzania danych.
